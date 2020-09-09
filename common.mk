8b9fcd2777a52684350bbd23a5ee4312288cb38f
Herobuxx <herobuxx@gmail.com>
2020-09-10 01:11:55 +0700

    msm8226: build hidl base and hidl manager

--- common.mk
+++ common.mk
@@ -186,6 +186,10 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
+
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := false
 
