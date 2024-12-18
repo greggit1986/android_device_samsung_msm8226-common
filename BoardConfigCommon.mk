53baeaf9d892b500364cfc15edf5992573b64b3c
greg <greg.jwaerhouse@gmail.com>
2024-12-18 05:21:17 -0500

    ***Drop Wifi for now to fix the build***

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -128,22 +128,22 @@ BOARD_VNDK_VERSION := current
 BOARD_AVB_ENABLE := false
 
 # Wifi
-BOARD_WLAN_DEVICE                := qcwcn
-BOARD_HAS_QCOM_WLAN              := true
-BOARD_HAS_QCOM_WLAN_SDK          := true
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-TARGET_PROVIDES_WCNSS_QMI        := true
-TARGET_USES_QCOM_WCNSS_QMI       := true
-TARGET_USES_WCNSS_CTRL           := true
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-WIFI_DRIVER_MODULE_PATH          := "/vendor/lib/modules/wlan.ko"
-WIFI_DRIVER_MODULE_NAME          := "wlan"
-WIFI_DRIVER_FW_PATH_STA          := "sta"
-WIFI_DRIVER_FW_PATH_AP           := "ap"
-WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
+#BOARD_WLAN_DEVICE                := qcwcn
+#BOARD_HAS_QCOM_WLAN              := true
+#BOARD_HAS_QCOM_WLAN_SDK          := true
+#BOARD_HOSTAPD_DRIVER             := NL80211
+#BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+#BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+#BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+#TARGET_PROVIDES_WCNSS_QMI        := true
+#TARGET_USES_QCOM_WCNSS_QMI       := true
+#TARGET_USES_WCNSS_CTRL           := true
+#WPA_SUPPLICANT_VERSION           := VER_0_8_X
+#WIFI_DRIVER_MODULE_PATH          := "/vendor/lib/modules/wlan.ko"
+#WIFI_DRIVER_MODULE_NAME          := "wlan"
+#WIFI_DRIVER_FW_PATH_STA          := "sta"
+#WIFI_DRIVER_FW_PATH_AP           := "ap"
+#WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 
 # inherit from the proprietary version
 include vendor/samsung/msm8226-common/BoardConfigVendor.mk
