7cd009301ed89a188699dc1e04cf23c3d40928af
Yaroslav Zviezda <acroreiser@gmail.com>
2024-12-18 22:44:01 +0300

    a6010: camera: enable manual exposure control for Camera2 API apps

--- common.mk
+++ common.mk
@@ -121,6 +121,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.hal3on1.use_limited_level=1 \
     persist.camera.hal3on1.use_memfd=1 \
     persist.camera.hal3on1.use_hwcomposer=1
+    persist.camera.hal3on1.use_manual_exposure=1
 ################################################################
 
 # Configstore
