af312bcaf3955d7dc63a01c8a434039575c4a1fa
greg <greg.jwaerhouse@gmail.com>
2025-03-07 21:16:34 -0500

    common.mk: Bring back wifi props.

--- common.mk
+++ common.mk
@@ -393,14 +393,14 @@ PRODUCT_COPY_FILES += \
 #    prebuilts/vndk/v30/arm/arch-arm-armv7-a-neon/shared/vndk-core/android.hardware.light-V1-ndk_platform.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.hardware.light-V1-ndk_platform.so
 
 # Wifi
-#PRODUCT_PACKAGES += \
-    #android.hardware.wifi-service \
-    #hostapd \
-    #wpa_supplicant \
-    #wpa_supplicant.conf \
-    #libwpa_client \
-    #android.hardware.wifi.supplicant-V1-ndk \
-    #android.hardware.wifi.hostapd-V1-ndk
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service-lazy.legacy \
+    hostapd \
+    wpa_supplicant \
+    wpa_supplicant.conf \
+    libwpa_client \
+    android.hardware.wifi.supplicant-V1-ndk \
+    android.hardware.wifi.hostapd-V1-ndk
 
     #android.hardware.wifi@1.0-service-lazy.legacy \ - msm8974
 
@@ -408,10 +408,10 @@ PRODUCT_COPY_FILES += \
 #PRODUCT_PACKAGES += \
 #    dhcpcd.conf
 
-#PRODUCT_PACKAGES += \
-#    libcurl \
-#    libwcnss_qmi \
-#    wcnss_service
+PRODUCT_PACKAGES += \
+    libcurl \
+    libwcnss_qmi \
+    wcnss_service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
