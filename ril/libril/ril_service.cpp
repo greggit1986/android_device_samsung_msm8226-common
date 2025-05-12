5787d3290d35fae2610d8d3304a5b37e46fa7e3f
greg <greg.jwaerhouse@gmail.com>
2025-05-12 19:34:21 -0400

    msm8226-common: gprimelte inspired RIL implementation: define MAX_BROADCAST_SMS_CONFIG_INFO to fix the build.

--- ril/libril/ril_service.cpp
+++ ril/libril/ril_service.cpp
@@ -42,6 +42,8 @@ extern "C" {
 
 #define INVALID_HEX_CHAR 16
 
+#define MAX_BROADCAST_SMS_CONFIG_INFO 25
+
 using namespace android::hardware::radio;
 using namespace android::hardware::radio::V1_0;
 using namespace android::hardware::radio::deprecated::V1_0;
