ea2d3767658c79a0ffea3eb4bc5d34795e0a078e
greg <greg.jwaerhouse@gmail.com>
2025-06-14 07:28:07 -0400

    hwcomposer: Use android.hardware.graphics.composer@2.1-service and explicitely include libhwc2on1adapter and libhwc2onfbadapter which are apparently now required to boot A12!?

--- msm8226.mk
+++ msm8226.mk
@@ -73,13 +73,16 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     copybit.msm8226 \
     gralloc.msm8226 \
     hwcomposer.msm8226 \
-    memtrack.msm8226
+    memtrack.msm8226 \
+    libhwc2on1adapter \
+    libhwc2onfbadapter
 
 # Camera
 PRODUCT_PACKAGES += \
