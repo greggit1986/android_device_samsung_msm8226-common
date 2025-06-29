a26e331168d21fecfab98f9a47f3bbfe9e2c5970
greg <greg.jwaerhouse@gmail.com>
2025-06-29 09:32:17 -0400

    msm8226-common: Ship with grapheneos camera version 77 https://github.com/GrapheneOS/Camera/releases/tag/77

--- msm8226.mk
+++ msm8226.mk
@@ -86,7 +86,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
     camera.msm8226 \
-    libxml2
+    libxml2 \
+    GrapheneCamera
 
 # DRM
 PRODUCT_PACKAGES += \
