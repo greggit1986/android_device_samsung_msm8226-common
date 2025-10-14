8f373900b67c9c9b066c21cd9e38457eba20890b
iusmac <iusico.maxim@libero.it>
2025-10-14 21:20:34 +0300

    msm8226-common: Switch WiFi driver into triple-mode configuration
    * This enables WiFi Interface Concurrency Combination support to operate
      simultaneously in STA (station/normal), AP (access point/hotspot) and
      P2P (WiFi Direct) modes.
    
    * NOTE: we hardcode just the STA mode in the WIFI_HAL_INTERFACE_COMBINATIONS
      array in order to compile the WiFi AIDL service to operate in V3 mode,
      also, it's the default mode that's checked by the Android when
      configuring the chip at WifiHAL startup. The actual full advertised
      list of supported Iface combinations will be dynamically loaded from
      the driver right after the first attempt to configure in STA mode. The
      following is expected to be logged:
    
          I android.hardware.wifi-service Reloading iface concurrency combination from driver
          I android.hardware.wifi-service Configured chip in mode 3
    
    * FYI: These are the supported combinations advertised by the driver:
    
          V WifiHAL  --- DUMP Interface Combination ---
          V WifiHAL  num_iface_combinations: 3
          V WifiHAL  comb1 : max_ifaces: 1 iface_limit: 1
          V WifiHAL      max=1, iface:[ STA AP P2P ]
          V WifiHAL  comb2 : max_ifaces: 3 iface_limit: 3
          V WifiHAL      max=3, iface:[ STA ]
          V WifiHAL      max=1, iface:[ AP ]
          V WifiHAL      max=1, iface:[ P2P ]
          V WifiHAL  comb3 : max_ifaces: 2 iface_limit: 1
          V WifiHAL      max=1, iface:[ STA ]
    
    * Since our wifi prima driver is statically compiled, it can only be
      loaded (aka insmod-ed) by modifying the module parameter
      /sys/module/wlan/parameters/fwpath from userspace, which is normally
      done when switching between STA and AP modes in V1 (fixed single
      interface) mode. But the V3 mode configuration doesn't require
      involving explicit firmware reloading in the driver, as switching is
      done "gracefully" via WIPHY interface. From now on, we'll be using the
      fwpath node as a "control" path to load the driver at WifiHAL startup.
      This also allows to preserve backwards compatibility in case of manual
      kernel (boot.img) swap.
      NOTE: the fwpath node doesn't expect a specific value, so an empty
      string is also valid.
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>
    Change-Id: Ic9cfaa1d478f91e40903752165de2732c81b138e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -241,8 +241,10 @@ TARGET_PROVIDES_WCNSS_QMI        := true
 TARGET_USES_QCOM_WCNSS_QMI       := true
 TARGET_USES_WCNSS_CTRL           := true
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
-WIFI_DRIVER_FW_PATH_STA          := "sta"
-WIFI_DRIVER_FW_PATH_AP           := "ap"
+WIFI_HAL_INTERFACE_COMBINATIONS := {{{STA}, 1}}
+WIFI_DRIVER_STATE_CTRL_PARAM := "/sys/module/wlan/parameters/fwpath"
+WIFI_DRIVER_STATE_ON := ""
+WIFI_DRIVER_STATE_OFF := ""
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 
 # inherit from the proprietary version
