e18168e00873b1e96a41c27a1646ae577aa790d1
basamaryan <basam.aryan@gmail.com>
2025-10-23 22:59:01 +0300

    msm8226-common: Override kernel BPF version

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -184,6 +184,8 @@ BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 # Properties
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
+TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 
 # Qualcomm support
 BOARD_USES_QCOM_HARDWARE := true
