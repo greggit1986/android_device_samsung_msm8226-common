9a7e57bc98c1a998e78a84acc3ab82be6f286825
greg <greg.jwaerhouse@gmail.com>
2026-07-26 18:18:20 -0400

    Camera: hal3on1: drop the following properties. It turns out one is responsible for the greenish tint on video recording output. More investigation to follow.     persist.camera.cpp.duplication=false \     persist.camera.hal.debug.mask=0 \     persist.camera.feature.cac=1 \     persist.camera.no-display=1 \     persist.camera.hal3on1.use_limited_level=1 \     persist.camera.hal3on1.use_memfd=0 \

--- common.mk
+++ common.mk
@@ -93,12 +93,6 @@ PRODUCT_PACKAGES += \
     GrapheneCamera
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.cpp.duplication=false \
-    persist.camera.hal.debug.mask=0 \
-    persist.camera.feature.cac=1 \
-    persist.camera.no-display=1 \
-    persist.camera.hal3on1.use_limited_level=1 \
-    persist.camera.hal3on1.use_memfd=0 \
     persist.camera.hal3on1.use_hwcomposer=1 \
     persist.camera.hal3on1.use_sysfs_torch=1
 
